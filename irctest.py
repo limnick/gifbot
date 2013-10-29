d9610364c8370fad8821f90b47706729156b1db7
Nick Johnson <njohnson@limcollective.com>
2013-10-28 23:06:58 -0700

    working version

---
+++ irctest.py
@@ -0,0 +1,118 @@
+import logging
+import re
+import socket
+import ssl
+
+import tornado.ioloop
+import tornado.iostream
+
+IRC_DISCONNECTED = 0
+IRC_NICK = 1
+IRC_CONNECTING = 2
+IRC_CONNECTED = 3
+
+PING_RE=re.compile('PING (?P<message>.+)')
+CHANMSG_RE=re.compile(':(?P<username>[^!]+)!(?P<who>[^ ]+) PRIVMSG (?P<chan>#[^ ]+) :(?P<msg>.*)')
+PRIVMSG_RE=re.compile(':(?P<username>[^!]+)!(?P<who>[^ ]+) PRIVMSG (?P<user>[^#][^ ]*) :(?P<msg>.*)')
+ERROR_RE=re.compile('ERROR :(?P<msg>.*)')
+
+
+class IRCConn(object):
+    def __init__(self, nickname, full_name="Tornado IRC", io_loop=None):
+        if not io_loop:
+            io_loop = tornado.ioloop.IOLoop.instance()
+        self.nickname = nickname
+        self.io_loop = io_loop
+        self.full_name = full_name
+        self.conn = None
+        self._state = IRC_DISCONNECTED
+
+    def on_connect(self):
+        """Callback that is invoked after connection"""
+        pass
+
+    def on_chanmsg(self, channel, username, message):
+        """Callback that is invoked on every channel message"""
+        pass
+
+    def on_privmsg(self, username, message):
+        """Callback taht is invoked on every private message"""
+        pass
+
+    def connect(self, host, port, do_ssl=False, password=None):
+        sock = None
+        self._last_connection = (host, port, do_ssl, password)
+        self._password = password
+        for (family, socktype, proto, canonname, sockaddr) in socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM, 0):
+            try:
+                fd = socket.socket(family, socktype, proto)
+                fd.connect(sockaddr)
+                fd.setblocking(0)
+                sock = fd
+                break
+            except socket.error:
+                pass
+        if not sock:
+            raise socket.error("Unable to connect to %s:%s" % (host, port))
+        if do_ssl:
+            sock = ssl.wrap_socket(sock, server_side=False, do_handshake_on_connect=False)
+            self.conn = tornado.iostream.SSLIOStream(sock, io_loop=self.io_loop)
+        else:
+            self.conn = tornado.iostream.IOStream(sock, io_loop=self.io_loop)
+        self.conn.read_until("\n", self._handle_data)
+
+    def _write(self, data, *args, **kwargs):
+        logging.debug('<<< %s', data)
+        self.conn.write(data + '\r\n', *args, **kwargs)
+
+    def _handle_data(self, data):
+        print ">>>", data.rstrip()
+        logging.debug(">>> %s", data.rstrip())
+        ping_md = PING_RE.match(data)
+        if ping_md:
+            self._write("PONG " + ping_md.group('message'))
+        if self._state == IRC_DISCONNECTED:
+            if self._password:
+                self._write("PASS %s" % self._password)
+            self._state = IRC_NICK
+        elif self._state == IRC_NICK:
+            self._write("NICK %s" % self.nickname)
+            self._write("USER %s 8 *  :%s" % (self.nickname, self.full_name))
+            self._state = IRC_CONNECTING
+        elif self._state == IRC_CONNECTING:
+            self.on_connect()
+            self._state = IRC_CONNECTED
+        elif self._state == IRC_CONNECTED:
+            cmd = CHANMSG_RE.match(data)
+            if cmd:
+                self.on_chanmsg(cmd.group('chan'), cmd.group('username'), cmd.group('msg'))
+            pmd = PRIVMSG_RE.match(data)
+            if pmd:
+                if pmd.group('user') == self.nickname:
+                    self.on_privmsg(pmd.group('username'), pmd.group('msg'))
+            emd = ERROR_RE.match(data)
+            if emd:
+                if "Closing Link" in emd.group('msg'):
+                    self.conn.close()
+                    self.connect(*self._last_connection)
+                    return
+        self.conn.read_until("\n", self._handle_data)
+
+    def join(self, channel):
+        if not channel.startswith("#"):
+            channel = "#" + channel
+        self._write("JOIN " + channel)
+
+    def chanmsg(self, channel, message):
+        for line in message.split("\n"):
+            self._write("PRIVMSG %s :%s" % (channel, line))
+
+    def privmsg(self, user, message):
+        self.chanmsg(user, message)
+
+    def quit(self, message, callback=None):
+        def after_quit(*args, **kwargs):
+            self.conn.close()
+            if callback:
+                callback()
+        self._write("QUIT :%s" % message, callback=after_quit)
--- server.py
+++ server.py
@@ -2,26 +2,58 @@ import errno
 import functools
 import socket
 import time
+import datetime
+from collections import deque
 from images2gif import GifWriter
-# import gifwriter as gw
-# import gifWriter2 as gw2
 from io import BytesIO
 from tornado import ioloop, iostream
 from PIL import Image, ImageFont, ImageFile, ImageDraw, ImagePalette
 import gifplugin as GifImagePlugin
-# from gifplugin import i8,o8,i16,o16
-# import gifmaker as gm
+from irctest import IRCConn
+import os
+
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+TIME_STEP = 5
+MAX_TIME = 60
 
 MARGIN = 2
-FONT = ImageFont.truetype("anony.ttf",12)
+FONT = ImageFont.truetype(os.path.join(BASE_PATH, "anony.ttf"),12)
+streams = set()
+
+output_buffer = deque(["" for _ in range(7)])
+
+gifWriter = GifWriter()
+gifWriter.transparency = False
+
+def get_header_data():
+    rio = BytesIO()
+    img = Image.new("RGB", (600,100))
+    draw = ImageDraw.Draw(img)
+    draw.text((MARGIN,MARGIN), "asdf", (0,255,0), font=FONT)
+    img = img.convert("P")
+    img.encoderinfo = {}
+    palette = img.im.getpalette("RGB")[:768]
+    img.putpalette(palette)
+
+    # Write header
+    # Gather info
+    header = gifWriter.getheaderAnim(img)
+    appext = gifWriter.getAppExt(1) #num loops
+    # Write
+    rio.write(header)
+    rio.write(palette)
+    rio.write(appext)
+    return rio.getvalue()
+
+HEADER_DATA = get_header_data()
+
+def new_msg(msg):
+    output_buffer.append(msg.decode("utf-8", 'ignore'))
+    if len(output_buffer) > 7:
+        output_buffer.popleft()
 
 def connection_ready(sock, fd, events):
-    print "CONN READY"
-    gif = ""
-    with open('/Users/njohnson/Downloads/earf.gif') as fp:
-        gif = fp.read()
-    # print gif
-
     while True:
         try:
             connection, address = sock.accept()
@@ -29,107 +61,121 @@ def connection_ready(sock, fd, events):
             if e[0] not in (errno.EWOULDBLOCK, errno.EAGAIN):
                 raise
             return
+
         connection.setblocking(0)
-        stream = iostream.IOStream(connection)
-        stream.write("HTTP/1.0 200 OK\r\n")
-        stream.write("Content-Type: image/gif\r\n")
-        stream.write("\r\n")
-        # stream.write(gif, stream.close)
+        handle_connection(connection, address)
 
-        images = []
-        palettes = []
-        for i in range(7):
-            img = Image.new("RGB", (600,100))
-            draw = ImageDraw.Draw(img)
-            for j in range(i):
-                draw.text((MARGIN,MARGIN+(j*16)), "".join(chr(x) for x in range(33,200)), (0,255,0), font=FONT)
-            img = img.convert("P")
-            img.encoderinfo = {}
-            palette = img.im.getpalette("RGB")[:768]
-            img.putpalette(palette)
-            palettes.append(palette)
-            images.append(img)
+def _handle_headers(stream, data):
+    lines = data.split("\r\n")
+    request,headers = lines[0],lines[1:-2]
+    for header in headers:
+        try:
+            param,val = map(str.strip, header.split(":"))
+        except ValueError:
+            continue
 
-        # rio = BytesIO()
-        rio = stream
-        gifWriter = GifWriter()
-        gifWriter.transparency = False
-        loops = 1
-        xy = [(0,0) for im in images]
-        dispose = [2 for im in images]
-        duration = [3.0 for im in images]
-        globalPalette = palettes[0]
+        if param == "User-Agent":
+            ua = val.lower()
+            is_mobile = bool(sum([x in ua for x in ['android', 'webos', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone']]))
+            if is_mobile:
+                stream.close()
 
-        # Init
-        frames = 0
-        firstFrame = True
+def closestream(stream):
+    global streams
+    stream.write(";")
+    stream.close()
 
+def handle_connection(connection, address):
+    stream = iostream.IOStream(connection)
+    callback = functools.partial(_handle_headers, stream)
+    stream.read_until("\r\n\r\n", callback)
+    stream.write("HTTP/1.0 200 OK\r\n")
+    stream.write("Content-Type: image/gif\r\n")
+    stream.write("\r\n")
+    
+    stream.write(HEADER_DATA)
+    callback = functools.partial(closestream, stream)
+    ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=MAX_TIME), callback)
+    streams.add(stream)
 
-        
-        for im, palette in zip(images, palettes):
-        
-            if firstFrame:
-                # Write header
-        
-                # Gather info
-                header = gifWriter.getheaderAnim(im)
-                appext = gifWriter.getAppExt(loops)
-                print "WRITING GIF HEADER"
-                # Write
-                rio.write(header)
-                rio.write(globalPalette)
-                rio.write(appext)
-        
-                # Next frame is not the first
-                firstFrame = False
-        
-            if True:
-                # Write palette and image data
-        
-                # Gather info
-                data = GifImagePlugin.getdata(im)
-                imdes, data = data[0], data[1:]
+def send_latest():
+    global streams
+    latest_gif = get_img_frame()
 
-                transparent_flag = 0
-                
-                graphext = gifWriter.getGraphicsControlExt(duration[frames],
-                    dispose[frames],transparent_flag=transparent_flag,transparency_index=255)
+    new_streams = set()
+    for stream in streams:
+        if not stream.closed():
+            stream.write(latest_gif)
+            new_streams.add(stream)
+    streams = new_streams
 
-                # Make image descriptor suitable for using 256 local color palette
-                lid = gifWriter.getImageDescriptor(im, xy[frames])
-        
-                # Write local header
-                # Use global color palette
-                print "WRITING GIF FRAME"
-                rio.write(graphext)
-                rio.write(imdes) # write suitable image descriptor
-        
-                # Write image data
-                for d in data:
-                    rio.write(d)
+def gen_img():
+    img = Image.new("RGB", (600,100))
+    draw = ImageDraw.Draw(img)
+    for i,line in enumerate(output_buffer):
+        draw.text((MARGIN,MARGIN+(i*14)), line, (0,255,0), font=FONT)
+    img = img.convert("P")
+    img.encoderinfo = {}
+    palette = img.im.getpalette("RGB")[:768]
+    img.putpalette(palette)
+    return img, palette
 
-                # print len(rio.getvalue())
-                import time;time.sleep(3)
-        
-            # Prepare for next round
-            frames = frames + 1
-        
-        stream.write(";")  # end gif
+def get_img_frame():
+    img, palette = gen_img()
+    rio = BytesIO()
+    
+    # Write palette and image data
+    # Gather info
+    data = GifImagePlugin.getdata(img)
+    imdes, data = data[0], data[1:]
+    graphext = gifWriter.getGraphicsControlExt(duration=TIME_STEP)
 
-        stream.close()
+    # Write local header
+    # Use global color palette
+    rio.write(graphext)
+    rio.write(imdes) # write suitable image descriptor
+
+    # Write image data
+    for d in data:
+        rio.write(d)
+    
+    return rio.getvalue()
  
+def chanmsg(self, channel, username, message):
+    new_msg("{}: {}".format(username, message))
+    print repr(message)
+    if message.startswith("!watchers"):
+        msg = "There are {} open sockets. ".format(len(streams))
+        self.chanmsg("#yospos", msg)
+    print "CHANMSG ", channel, username, message, ""
+
 if __name__ == '__main__':
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setblocking(0)
-    sock.bind(("", 8010))
+    sock.bind(("0.0.0.0", 80))
     sock.listen(5000)
- 
+
     io_loop = ioloop.IOLoop.instance()
     callback = functools.partial(connection_ready, sock)
     io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
+
+    irc = IRCConn("gifbot", "gifbot", io_loop)
+    irc.on_chanmsg = functools.partial(chanmsg, irc)
+    irc.connect('irc.synirc.net', 6667)
+
+    def joinirc(irc):
+        irc.join("#yospos")
+    cb = functools.partial(joinirc, irc)
+
+    io_loop.add_timeout(datetime.timedelta(seconds=10), cb)
+
+    send_loop = ioloop.PeriodicCallback(send_latest, TIME_STEP*1000)
+    send_loop.start()
     try:
         io_loop.start()
     except KeyboardInterrupt:
+        send_loop.stop()
         io_loop.stop()
         print "exited cleanly"
