e84245c5db81fe528112b6abe0e4bf9974926749
Nick Johnson <njohnson@limcollective.com>
2015-12-31 18:08:13 -0800

    adding multiple image support

--- server.py
+++ server.py
@@ -25,9 +25,6 @@ DO_SSL = True
 FRAME_SPINNER = "------"
 SPINNER_LEN = len(FRAME_SPINNER)
 
-IMG_WIDTH = 600
-IMG_HEIGHT = 120
-
 BIND_ADDR = "0.0.0.0"
 BIND_PORT = 9000
 
@@ -40,20 +37,30 @@ LINE_SPACING = 14
 MARGIN = 2
 FONT = ImageFont.truetype(os.path.join(BASE_PATH, "anony.ttf"), 12)
 
-TOTAL_NUM_LINES = (IMG_HEIGHT / LINE_SPACING) - 1
-print "tot", TOTAL_NUM_LINES
+IMAGE_DEFS = {
+    'sig.gif': (600,120),
+    'site.gif': (600,400),
+}
+DEFAULT_IMG_NAME = 'sig.gif'
 
-streams = set()
-LAST_FRAME,HEADER_DATA = None,None
-output_buffer = deque(["" for _ in range(TOTAL_NUM_LINES)])
+BUFFER_MAX = 50
+output_buffer = ["" for _ in range(BUFFER_MAX)]
 
 gifWriter = GifWriter()
 gifWriter.transparency = False
 
+streams = {}
+LAST_FRAME = {}
+HEADER_DATA = {}
+for img_name in IMAGE_DEFS.keys():
+    streams[img_name] = set()
+    LAST_FRAME[img_name] = None
+    HEADER_DATA[img_name] = None
 
-def get_header_data():
+
+def get_header_data(width,height):
     rio = BytesIO()
-    img,palette = gen_img()
+    img,palette = gen_img(width,height)
 
     header = gifWriter.getheaderAnim(img)
     appext = gifWriter.getAppExt(1) #num loops
@@ -69,9 +76,9 @@ def new_msg(msg):
     if raw_msg.startswith("***"):
         return
 
-    output_buffer.append(raw_msg)
-    if len(output_buffer) > TOTAL_NUM_LINES:
-        output_buffer.popleft()
+    output_buffer.insert(0, raw_msg)
+    if len(output_buffer) > BUFFER_MAX:
+        output_buffer.pop()
 
 
 def connection_ready(sock, fd, events):
@@ -91,6 +98,11 @@ def _handle_headers(stream, data):
     lines = data.split("\r\n")
     request, headers = lines[0], lines[1:-2]
     print request
+    cur_img_name = DEFAULT_IMG_NAME
+    for img_name in IMAGE_DEFS.keys():
+        if request.startswith("GET /{}".format(img_name)):
+            cur_img_name = img_name
+
     for header in headers:
         try:
             param,val = map(str.strip, header.split(":"))
@@ -103,6 +115,17 @@ def _handle_headers(stream, data):
             if is_mobile:
                 stream.close()
 
+    stream.write("HTTP/1.0 200 OK\r\n")
+    stream.write("Content-Type: image/gif\r\n")
+    stream.write("\r\n")
+
+    stream.write(HEADER_DATA[cur_img_name])
+    stream.write(LAST_FRAME[cur_img_name])
+    if CLOSE_ON_TIMEOUT:
+        callback = functools.partial(closestream, stream)
+        ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=MAX_TIME), callback)
+    streams[cur_img_name].add(stream)
+
 
 def closestream(stream):
     if not stream.closed():
@@ -114,42 +137,37 @@ def handle_connection(connection, address):
     stream = iostream.IOStream(connection)
     callback = functools.partial(_handle_headers, stream)
     stream.read_until("\r\n\r\n", callback)
-    stream.write("HTTP/1.0 200 OK\r\n")
-    stream.write("Content-Type: image/gif\r\n")
-    stream.write("\r\n")
-
-    stream.write(HEADER_DATA)
-    stream.write(LAST_FRAME)
-    if CLOSE_ON_TIMEOUT:
-        callback = functools.partial(closestream, stream)
-        ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=MAX_TIME), callback)
-    streams.add(stream)
 
 
 def send_latest():
     global streams, LAST_FRAME, FRAMECOUNT
-    latest_gif = get_img_frame()
-
     FRAMECOUNT += 1
     if FRAMECOUNT >= SPINNER_LEN:
         FRAMECOUNT = 0
 
-    del LAST_FRAME
-    LAST_FRAME = latest_gif
+    for img_name,img_size in IMAGE_DEFS.items():
+        latest_gif = get_img_frame(*img_size)
 
-    new_streams = set()
-    for stream in streams:
-        if not stream.closed():
-            stream.write(latest_gif)
-            new_streams.add(stream)
+        del LAST_FRAME[img_name]
+        LAST_FRAME[img_name] = latest_gif
 
-    del streams
-    streams = new_streams
+        new_streams = set()
+        for stream in streams[img_name]:
+            if not stream.closed():
+                stream.write(latest_gif)
+                new_streams.add(stream)
+
+        del streams[img_name]
+        streams[img_name] = new_streams
 
 
-def gen_img():
-    img = Image.new("RGB", (IMG_WIDTH,IMG_HEIGHT))
+def gen_img(width,height):
+    img = Image.new("RGB", (width,height))
     draw = ImageDraw.Draw(img)
+
+    TOTAL_NUM_LINES = (height / LINE_SPACING) - 1
+
+    num_viewers = sum(len(x) for x in streams.values())
 
     spinner_pos = FRAMECOUNT % SPINNER_LEN
     frame_spinner = FRAME_SPINNER[:spinner_pos] + '*' + FRAME_SPINNER[spinner_pos+1:]
@@ -157,22 +175,22 @@ def gen_img():
     draw.text(
         (MARGIN,MARGIN+(LINE_SPACING*TOTAL_NUM_LINES)),
         "---- [{viewerstr: ^18s}] {frame_spinner:s} {chan: ^14s} -- {server: ^18s} ----".format(**{
-            'viewerstr': str(len(streams))+" viewers",
+            'viewerstr': "{} viewers".format(num_viewers),
             'frame_spinner': frame_spinner,
             'chan': IRC_CHAN,
             'server': PUB_IRC_NETWORK,
         }),
         (0,255,0),
         font=FONT)
-    for i,line in enumerate(output_buffer):
+    for i,line in enumerate(reversed(output_buffer[:TOTAL_NUM_LINES])):
         draw.text((MARGIN,MARGIN+(i*LINE_SPACING)), line, (0,255,0), font=FONT)
     img = img.convert("P")
     palette = img.im.getpalette("RGB")[:768]
     return img, palette
 
 
-def get_img_frame():
-    img, palette = gen_img()
+def get_img_frame(width,height):
+    img, palette = gen_img(width,height)
     rio = BytesIO()
 
     data = GifImagePlugin.getdata(img)
@@ -198,8 +216,9 @@ def chanmsg(self, channel, username, message):
     # print "CHANMSG ", channel, username, message, ""
 
 if __name__ == '__main__':
-    LAST_FRAME = get_img_frame()
-    HEADER_DATA = get_header_data()
+    for img_name,img_size in IMAGE_DEFS.items():
+        LAST_FRAME[img_name] = get_img_frame(*img_size)
+        HEADER_DATA[img_name] = get_header_data(*img_size)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
